b557b73fb784663b8e91372e810120715275074b
王建 <1556879688@qq.com>
2021-07-30 19:08:59 +0800

    Initial commit

---
+++ src/main/java/com/ivo/MyivoApplication.java
@@ -0,0 +1,17 @@
+package com.ivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wj
+ * @version 1.0
+ */
+@SpringBootApplication
+public class MyivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyivoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ivo/home/HomeController.java
@@ -0,0 +1,21 @@
+package com.ivo.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Myivo 主页面
+ * @author wj
+ * @version 1.0
+ */
+@Controller
+public class HomeController {
+
+    /** 主页面 **/
+    private static final String HOME_PAGE_URL = "page/home.html";
+
+    @RequestMapping(value = {"/", "/home"})
+    public String home() {
+        return HOME_PAGE_URL;
+    }
+}
